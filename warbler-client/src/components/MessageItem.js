97ad62e81ca260f53b3b8dda97f0d6459519809a
Rob Tucker <robt9095@gmail.com>
2018-04-05 23:42:13 -0700

    Add files via upload

--- warbler-client/src/components/MessageItem.js
+++ warbler-client/src/components/MessageItem.js
@@ -3,7 +3,7 @@ import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import DefaultProfileImg from '../images/default-profile-image.jpg';
 
-const MessageItem = ({date, profileImageUrl, text, username, removeMessage, followUser, isCorrectUser }) => (
+const MessageItem = ({isFollowing, date, profileImageUrl, text, username, removeMessage, followUser, isCorrectUser }) => (
   <div>
     <li className="list-group-item">
       <img src={profileImageUrl || DefaultProfileImg} alt={username} height="100" width="100" className="timeline-image" />
@@ -15,11 +15,14 @@ const MessageItem = ({date, profileImageUrl, text, username, removeMessage, foll
           </Moment>
         </span>
         <p>{text}</p>
-        {isCorrectUser ?
-			<a className="btn btn-danger" onClick={removeMessage}>Delete</a> 
-		  : 
-			<a className="btn btn-success" onClick={followUser}>Follow</a>
-		}
+        {
+          isCorrectUser ?
+            <a className="btn btn-danger" onClick={removeMessage}>Delete</a> :
+              isFollowing ?
+                  <a className="btn btn-success" onClick={followUser}>Un-Follow</a>
+                :
+                  <a className="btn btn-success" onClick={followUser}>Follow</a>
+		    }
       </div>
     </li>
 </div>
