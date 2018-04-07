266543df7d6f915e76913e36029b4d4bb4cd9661
Rob Tucker <robt9095@gmail.com>
2018-04-06 17:05:49 -0700

    Add files via upload

--- warbler-client/src/store/reducers/followers.js
+++ warbler-client/src/store/reducers/followers.js
@@ -1,12 +1,10 @@
-import { ADD_FOLLOWER, LOAD_FOLLOWERS } from '../actionTypes';
+import { LOAD_FOLLOWERS } from '../actionTypes';
 
 
 const followers = (state = [], action) => {
   switch(action.type) {
     case LOAD_FOLLOWERS:
       return [...action.followers];
-    case ADD_FOLLOWER:
-      return [...action.follower];
     default:
       return state;
   }
--- warbler-client/src/store/reducers/following.js
+++ warbler-client/src/store/reducers/following.js
@@ -1,9 +1,14 @@
-import { LOAD_FOLLOWING } from '../actionTypes';
+import { LOAD_FOLLOWING, ADD_FOLLOWER, REMOVE_FOLLOWER } from '../actionTypes';
 
 const following = (state = [], action) => {
   switch(action.type) {
     case LOAD_FOLLOWING:
       return [...action.following];
+    case ADD_FOLLOWER:
+	  let newState = [...state, action.userFollowed_id];
+      return newState;
+    case REMOVE_FOLLOWER:
+      return state.filter(follower => follower != action.userFollowed_id);	  	  
     default:
       return state;
   }
